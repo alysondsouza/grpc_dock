e8f4da753625716ced90f2590e98efc09cf17b3a
Alyson De Souza <alysondsouza@yahoo.com>
2021-10-13 16:16:55 +0200

    init

---
+++ server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"GO_GRPC_CHAT/chat"	//?
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChittyChatServiceServer(grpcServer, &s)
+	
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+
+}
